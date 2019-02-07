e35c6b899c051abca1789c45ee538c458500da9a
“Krishna” <iamkrishv@gmail.com>
2019-02-07 17:27:47 +0100

    refactored the field

--- src/main/java/com/weatherapp/service/WeatherAppService.java
+++ src/main/java/com/weatherapp/service/WeatherAppService.java
@@ -4,7 +4,7 @@ import com.weatherapp.customexception.WeatherException;
 import com.weatherapp.model.WeatherData;
 
 public interface WeatherAppService {
-	public final String BRUSSELS_WHEATHER="https://openweathermap.org/data/2.5/weather/?appid=b6907d289e10d714a6e88b30761fae22&lat=50.8503&lon=4.3517";
+	public static final String BRUSSELS_WHEATHER="https://openweathermap.org/data/2.5/weather/?appid=b6907d289e10d714a6e88b30761fae22&lat=50.8503&lon=4.3517";
 	public  WeatherData fetchweatherInfo() throws WeatherException ;
 	
 }
