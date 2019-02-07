4bf9f4fe44aeb679cdb4da4c07a36d28cea965e3
“Krishna” <iamkrishv@gmail.com>
2019-02-07 12:50:40 +0100

    Added testweatherservice function to validate the  response status code

---
+++ src/test/java/com/weatherapp/service/WeatherAppServiceImplTest.java
@@ -0,0 +1,41 @@
+package com.weatherapp.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+@RunWith(MockitoJUnitRunner.class)
+public class WeatherAppServiceImplTest {
+
+	@Mock
+	private RestTemplate restTemplate;
+	
+	@InjectMocks
+	private WeatherAppServiceImpl weatherAppService;
+	
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void testWeatherService() {
+		TestRestTemplate testRestTemplate = new TestRestTemplate();
+		ResponseEntity<String> response = testRestTemplate.getForEntity(WeatherAppService.BRUSSELS_WHEATHER, String.class);
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertNotNull(response.getBody());
+	}
+	
+
+}
