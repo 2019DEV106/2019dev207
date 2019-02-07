3f1a60c239eaad0d5101b45ceae4f7c4e14d148e
“Krishna” <iamkrishv@gmail.com>
2019-02-07 14:15:04 +0100

    updated weatherData fields

--- src/main/java/com/weatherapp/model/WeatherData.java
+++ src/main/java/com/weatherapp/model/WeatherData.java
@@ -1,20 +1,158 @@
 package com.weatherapp.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class WeatherData
 {
-   
+    private String dt;
+
+    @JsonProperty("coord")
+    private Coord coord;
+
+    private String visibility;
+
+    private Weather[] weather;
 
     private String name;
 
-	public String getName() {
-		return name;
-	}
+    private String cod;
 
-	public void setName(String name) {
-		this.name = name;
-	}
-  
-    
-    
+    private Main main;
+
+    private Clouds clouds;
+
+    private String id;
+
+    private Sys sys;
+
+    private String base;
+
+    private Wind wind;
+
+    public String getDt ()
+    {
+        return dt;
+    }
+
+    public void setDt (String dt)
+    {
+        this.dt = dt;
+    }
+
+    public Coord getCoord ()
+    {
+        return coord;
+    }
+
+    public void setCoord (Coord coord)
+    {
+        this.coord = coord;
+    }
+
+    public String getVisibility ()
+    {
+        return visibility;
+    }
+
+    public void setVisibility (String visibility)
+    {
+        this.visibility = visibility;
+    }
+
+    public Weather[] getWeather ()
+    {
+        return weather;
+    }
+
+    public void setWeather (Weather[] weather)
+    {
+        this.weather = weather;
+    }
+
+    public String getName ()
+    {
+        return name;
+    }
+
+    public void setName (String name)
+    {
+        this.name = name;
+    }
+
+    public String getCod ()
+    {
+        return cod;
+    }
+
+    public void setCod (String cod)
+    {
+        this.cod = cod;
+    }
+
+    public Main getMain ()
+    {
+        return main;
+    }
+
+    public void setMain (Main main)
+    {
+        this.main = main;
+    }
+
+    public Clouds getClouds ()
+    {
+        return clouds;
+    }
+
+    public void setClouds (Clouds clouds)
+    {
+        this.clouds = clouds;
+    }
+
+    public String getId ()
+    {
+        return id;
+    }
+
+    public void setId (String id)
+    {
+        this.id = id;
+    }
+
+    public Sys getSys ()
+    {
+        return sys;
+    }
+
+    public void setSys (Sys sys)
+    {
+        this.sys = sys;
+    }
+
+    public String getBase ()
+    {
+        return base;
+    }
+
+    public void setBase (String base)
+    {
+        this.base = base;
+    }
+
+    public Wind getWind ()
+    {
+        return wind;
+    }
+
+    public void setWind (Wind wind)
+    {
+        this.wind = wind;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [dt = "+dt+", coord = "+coord+", visibility = "+visibility+", weather = "+weather+", name = "+name+", cod = "+cod+", main = "+main+", clouds = "+clouds+", id = "+id+", sys = "+sys+", base = "+base+", wind = "+wind+"]";
+    }
 }
 		
