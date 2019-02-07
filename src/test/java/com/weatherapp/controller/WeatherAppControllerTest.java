d2ac0b2e59912b01d1c2ba03316913881c927b31
“Krishna” <iamkrishv@gmail.com>
2019-02-07 12:26:33 +0100

    Added unit testcase to check weather api response is not null

--- src/test/java/com/weatherapp/controller/WeatherAppControllerTest.java
+++ src/test/java/com/weatherapp/controller/WeatherAppControllerTest.java
@@ -1,5 +1,10 @@
 package com.weatherapp.controller;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -8,6 +13,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -15,6 +21,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.ui.ModelMap;
 
 import com.weatherapp.model.WeatherData;
 import com.weatherapp.service.WeatherAppServiceImpl;
@@ -27,6 +34,9 @@ public class WeatherAppControllerTest {
 	@MockBean
 	public WeatherAppServiceImpl weatherAppServiceImpl;
 	   
+	@Mock
+	public WeatherAppServiceImpl weatherAppServiceImplMock;
+	
 	 
 	@InjectMocks
 	private WeatherAppController weatherAppController;
@@ -46,6 +56,17 @@ public class WeatherAppControllerTest {
 				.andExpect(status().isOk());
 	}
 	
-	 
-	 
+	@Test
+	public void testnavigateWeatherInfoPage_responseNotNull() throws Exception{
+		WeatherData weatherData = new WeatherData();
+		weatherData.setName("Brussels");
+		when(weatherAppServiceImplMock.fetchweatherInfo()).thenReturn(weatherData);
+		ModelMap modelMap = new ModelMap();
+		String result = weatherAppController.navigateWeatherInfoPage(modelMap);
+		assertNotNull(modelMap.get("weatherData"));
+		assertEquals("weatherInfo", result);
+		assertEquals("Brussels", ((WeatherData) modelMap.get("weatherData")).getName());
+		verify(weatherAppServiceImplMock, times(1)).fetchweatherInfo();
+	}
 }
+	
